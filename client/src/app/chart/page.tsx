c1a16fe5bc9e877a895f71dcc726e9f8b5a1b18b
Evan Keane <web3evankeane@gmail.com>
2025-12-17 13:07:54 +0700

    Feat: Position Size Calculator

---
+++ client/src/app/chart/page.tsx
@@ -0,0 +1,11 @@
+import TradingViewWidget from "@/components/layouts/tradingViewWidget";
+
+function Chart() {
+  return (
+    <div className="min-h-100">
+      Chart
+      <TradingViewWidget />
+    </div>
+  );
+}
+export default Chart;
